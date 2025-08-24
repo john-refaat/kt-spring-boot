6708d6586813d81f7e814db1c5aaebead7f39e9f
John Stefanos <john.refaat88@gmail.com>
2025-08-25 00:55:17 +0300

    first commit

---
+++ src/test/kotlin/com/plcoding/ktspringboot/KtSpringBootApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.plcoding.ktspringboot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KtSpringBootApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
